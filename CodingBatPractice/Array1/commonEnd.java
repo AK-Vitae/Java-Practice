42bc708f393b2d1e4bb7b630bdb62dc5ac8fe0a8
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-02-29 17:54:59 -0500

    Refined directory names and made sure the class included the typical String[] args main method

---
+++ CodingBatPractice/Array1/commonEnd.java
@@ -0,0 +1,19 @@
+public class commonEnd{
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        boolean a = false;
+        int[] arr = new int[n];
+        int[] arr2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = (int)(Math.random() * 9);
+        }
+        for (int i = 0; i < n; i++) {
+            arr2[i] = (int)(Math.random() * 9);
+        }
+
+        if (arr[0]==arr2[0]||arr[n-1]==arr2[n-1]){
+            a = true;
+        }
+        System.out.println(a);
+    }
+}
