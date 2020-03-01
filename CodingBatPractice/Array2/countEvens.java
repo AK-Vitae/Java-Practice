a7cb5805a13dcf8d4dbcadb7e3d78531d8a235b0
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-02-29 22:04:31 -0500

    Program returns the number of even ints in a command line array input

---
+++ CodingBatPractice/Array2/countEvens.java
@@ -0,0 +1,12 @@
+//Return the number of even ints in the given array.
+public class countEvens{
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            if (Integer.parseInt(args[i])%2==0) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
