bc8d9e044e9cc481fa44b83f016d583d5d155ab5
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-02-29 23:00:01 -0500

    Program that will print true if command line values have either 3 even or 3 odd values all next to each other

---
+++ CodingBatPractice/Array2/modThree.java
@@ -0,0 +1,16 @@
+//Print true if command line values have either 3 even or 3 odd values all next to each other
+public class modThree{
+    public static void main(String[] args) {
+        boolean a = false;
+        if (args.length<3) {
+            System.out.println("Please enter an array that is 3 elements or larger");
+        } else {
+            for (int i = 0; i < args.length-2; i++) {
+                if ((Integer.parseInt(args[i])%2)==(Integer.parseInt(args[i+1])%2)&&(Integer.parseInt(args[i+1])%2)==(Integer.parseInt(args[i+2])%2)) {
+                    a = true;
+                }
+            }
+        }
+        System.out.println(a);
+    }
+}
