2d9ffe27ebfcf051a7d701ddeb7ab6e798c4169b
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-03-06 22:49:24 -0500

    GuessingGame instructions commented

--- src/practice/standard_library/GuessingGame.java
+++ src/practice/standard_library/GuessingGame.java
@@ -1,5 +1,9 @@
 package practice.standard_library;
-
+// Write a program which implements a Guessing Game.
+// Generate a random int between 1 to 1,000, and prompt the user to try to guess what the number is.
+// As the user enters numbers, the program should tell whether the guess is too high or too low.
+// Once the user guesses correctly, have the program output "You win!"
+// All input and output must be done using StdIn and StdOut libraries.
 public class GuessingGame {
     public static void main(String[] args) {
         int num = (int) (Math.random()*1000)+1;
