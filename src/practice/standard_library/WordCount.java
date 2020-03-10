ad0a8d1e27e70837c22ea66be0345fae651b41fe
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-03-09 20:49:26 -0400

    Use readLine as the primary counter for line count, and then use .split to calculate word and character count

--- src/practice/standard_library/WordCount.java
+++ src/practice/standard_library/WordCount.java
@@ -3,16 +3,23 @@ package practice.standard_library;
 import practice.standard.StdIn;
 import practice.standard.StdOut;
 //Return the number of lines, words, and characters
-public class WordCount {
-    public static void main(String[] args) {
-        int wordCount = 0;
-        int charCount = 0;
+public class WordCount
+{
+    public static void main(String [] args){
+        int words = 0, lines = 0, characters = 0;
         while (!StdIn.isEmpty()) {
-            String word = StdIn.readString();
-            wordCount++;
-            charCount+= word.length();
+            String line = StdIn.readLine();
+            lines++;
+            String[] lineWords = line.split("\\s+");
+            words += lineWords.length;
+
+            for (int i = 0; i < lineWords.length; i++) {
+                String word = lineWords[i];
+                characters += word.length();
+            }
         }
-        StdOut.println("number of words  = " + wordCount);
-        StdOut.println("number of characters  = " + charCount);
+        StdOut.println("There are " + lines + " lines");
+        StdOut.println("There are " + words + " words");
+        StdOut.println("There are " + characters + " characters");
     }
 }
