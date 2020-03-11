1ca5e78e293cbe0d465f28c8b6a3b740d5495a4c
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-03-11 13:02:02 -0400

    Added more method practice work

---
+++ src/practice/static_methods/GreatestCommonFactor.java
@@ -0,0 +1,22 @@
+package practice.static_methods;
+
+import practice.standard.StdIn;
+import practice.standard.StdOut;
+
+public class GreatestCommonFactor {
+    public static int gcf(int a, int b){
+        int factor = 0;
+        for (int i = 1; i <=a && i<=b; i++) {
+            if (a%i==0 && b%i==0) {
+                factor = i;
+            }
+        }
+        return factor;
+    }
+
+    public static void main(String[] args) {
+        int x = StdIn.readInt();
+        int y = StdIn.readInt();
+        StdOut.println(gcf(x,y));
+    }
+}
