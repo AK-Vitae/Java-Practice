d9fd08fd916978d007d09bede2d76e7fbb975a20
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-04-04 13:35:25 -0400

    Added more array practice work

---
+++ src/practice/arrays/HowMany.java
@@ -0,0 +1,7 @@
+package practice.arrays;
+//Takes a variable number of command-line arguments and prints how many there are
+public class HowMany {
+    public static void main(String[] args) {
+        System.out.println("There are "+args.length+" arguments");
+    }
+}
