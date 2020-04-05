405b214609437423a1752d10bd8c6370aafe5ff9
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-04-04 22:05:48 -0400

    Added more static methods practice

---
+++ src/practice/static_methods/Equal.java
@@ -0,0 +1,22 @@
+package practice.static_methods;
+
+public class Equal {
+    //Takes two int arrays and returns true if the arrays are the same length, and have the same corresponding values
+    public static boolean eq(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {1, 2, 4};
+        System.out.println(eq(arr1, arr2));
+    }
+}
