14010c3a82f1df78896c91844fcc9807ec79af69
AK <ak.ramadugu@gmail.com>
2020-06-02 21:57:04 -0400

    Test for mac IntelliJ set up

--- src/hackerrank/javabasic/IfElse.java
+++ src/hackerrank/javabasic/IfElse.java
@@ -11,7 +11,7 @@ public class IfElse {
 			System.out.println("Weird");
 		} else if (N % 2 == 0 && N>=2 && N<=5) {
 			System.out.println("Not Weird");
-		} else if (N % 2 == 0 && N>=6 && N<=20) {
+		} else if (N % 2 == 0 && N>=6 && N<=19) {
 			System.out.println("Weird");
 		} else {
 			System.out.println("Not Weird");
