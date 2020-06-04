a082eccb437e790f54078d1df2fa9c971da12c06
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-06-04 16:53:49 -0400

    Added dependency classes

---
+++ src/datastructures/linkedlists/IntNode.java
@@ -0,0 +1,20 @@
+package datastructures.linkedlists;
+
+public class IntNode {
+	public int data;
+	public IntNode next;
+	
+	public IntNode(int data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	public IntNode(int data, IntNode next) {
+		this.data = data;
+		this.next = next;
+	}
+	
+	public String toString() {
+		return data + "";
+	}
+}
