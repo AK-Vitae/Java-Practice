385303609c2c3ecdda1e5518d4e6f22886747404
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-06-16 21:40:41 -0400

    Added notes regarding queues

---
+++ src/datastructures/queues/Queue.java
@@ -0,0 +1,63 @@
+package datastructures.queues;
+
+import datastructures.linkedlists.doublyll.DoublyNode;
+
+//FIFO: First-In First-Out
+public class Queue<T> {
+    private DoublyNode<T> rear;
+    private DoublyNode<T> front;
+    private int size;
+
+    public Queue() { //Constructor
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+    // For visualization terms, front is on the left side with rear on the right.
+    public void enqueue(T item) {
+        DoublyNode<T> newNode = new DoublyNode<T>(item);
+        if (this.size == 0) {
+            this.front = newNode;
+        } else {
+            this.rear.setNext(newNode);
+        }
+        this.rear = newNode;
+        this.size++;
+
+    }
+
+    public T dequeue() {
+        if (this.size == 0) {
+            return null;
+        }
+        T data = front.getData();
+        front = front.getNext();
+        size--;
+        return data;
+    }
+
+    public void traverse() {
+        DoublyNode<T> current = this.front;
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.getNext();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> test = new Queue<Integer>();
+        test.enqueue(3);
+        test.enqueue(4);
+        test.enqueue(5);
+        test.traverse();
+        test.dequeue();
+        test.traverse();
+        test.dequeue();
+        test.traverse();
+        test.dequeue();
+        test.traverse();
+    }
+
+}
